8937e4bacc63e554429b1a484b00765fe0b631c1
Davemuriu <dmuriu0@gmail.com>
2026-01-15 10:30:46 +0300

    fix: Update MAIL_USE_TLS configuration to handle various truthy values

--- server/config.py
+++ server/config.py
@@ -23,7 +23,7 @@ class Config:
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') == 'True'
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() in ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
